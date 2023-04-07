9c4ceb8a9183fef355910f0a68b0d72ce2378ee5
PixelShell <luigi41luigi41@gmail.com>
2023-04-07 23:37:40 +0300

    basic movement on line

---
+++ Mitosis/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f; // The speed at which the player moves
+
+    void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal"); // Gets the player's horizontal input (A or D)
+
+        if (moveInput != 0)
+        {
+            transform.Translate(Vector2.right * moveInput * speed * Time.deltaTime); // Moves the player left or right
+        }
+    }
+}
